809eec9c905a90ed7b65a3ed28ccb217db68243f
mpyw <ryosuke_i_628@yahoo.co.jp>
2016-08-07 00:46:39 +0900

    Fixed bugs

--- src/BuiltinServerProcess.php
+++ src/BuiltinServerProcess.php
@@ -22,37 +22,21 @@ class BuiltinServerProcess
             $cmd = "lsof -i4TCP:$port | perl -ane '{if(\$F[8]=~/^[^:]+:(\d+)/){print \$1}}'";
             $outputs = $this->execCommand($cmd);
         } while (!empty($outputs));
-        $pid = $this->fork($port);
-        if (!empty($pid)) {
-            $this->pid = $pid;
-            $this->port = $port;
-            usleep(50000);
-            return;
+        $escdir = escapeshellarg($directory);
+        $pid = (int)current($this->execCommand("cd $escdir; php -S localhost:$port 1>&2 & printf \$!"));
+        if ($pid === 0) {
+            throw new \UnexpectedValueException;
         }
         $this->info("Listening on localhost:$port");
-        $escdir = escapeshellarg($directory);
-        $this->execCommand("cd $escdir; php -S localhost:$port >/dev/null 2>&1", false);
-        exit;
+        $this->pid = $pid;
+        $this->port = $port;
+        usleep(50000);
     }
 
     public function __destruct()
     {
-        if (empty($this->port)) {
-            return;
-        }
-        $search = "php -S localhost:$this->port";
-        $outputs = $this->execCommand("ps | grep '$search'");
-        foreach ($outputs as $item) {
-            if (
-                strpos($item, 'grep') === false &&
-                strpos($item, 'sh') === false &&
-                strpos($item, $search) !== false
-            ) {
-                $pid = (int)$item;
-                $this->execCommand("kill $pid 2>/dev/null");
-                $this->info("Killed localhost:$this->port");
-                return;
-            }
+        if ($this->pid !== 0) {
+            posix_kill($this->pid, SIGKILL);
         }
     }
 
