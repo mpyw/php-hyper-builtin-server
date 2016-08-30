b2f9db41f71a8f8d57c8208db57ac35857aed230
mpyw <ryosuke_i_628@yahoo.co.jp>
2016-08-31 08:22:51 +0900

    Gracefull shutdown

--- src/Internal/ConnectionHandler.php
+++ src/Internal/ConnectionHandler.php
@@ -49,7 +49,13 @@ class ConnectionHandler
         try {
             $child = new Stream($this->master->children[$i]->getSocketClient(), $this->master->loop);
             $conn->pipe($child);
-            $child->pipe($conn);
+            $child->on('data', function ($data) use ($conn) {
+                $conn->write($data);
+            });
+            $child->on('end', function () use ($conn) {
+                stream_socket_enable_crypto($conn->stream, false);
+                $conn->end();
+            });
             $child->on('close', function () use ($i) {
                 $this->master->using[$i] = false;
             });
